cd6250a13e236b62d8d48c50d8126fb148df4852
David <DavidJacobsen@davids-air.lan>
2020-04-08 17:32:03 -0400

    initial setup

---
+++ NumberGame/NumberGame/Controllers/GameController.swift
@@ -0,0 +1,17 @@
+//
+//  GameController.swift
+//  NumberGame
+//
+//  Created by David on 4/8/20.
+//  Copyright © 2020 David. All rights reserved.
+//
+
+import Foundation
+
+class GameController {
+    
+    static var scores = [Int]()
+
+    init (){
+    }
+}
